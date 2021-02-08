665b14297690b4df48c3885df92f1e2a6121c8a0
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2021-02-09 00:58:24 +0530

    idk

--- oth1-1819-back/checkpoint.php
+++ oth1-1819-back/checkpoint.php
@@ -1,9 +1,13 @@
 <?php
+<<<<<<< HEAD
 session_start();
+=======
+>>>>>>> 6279259c638314100928366b0120f5b5b2e3cf6f
 include 'dbconnect.php';
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 $user_id = $request->user_id;
+<<<<<<< HEAD
 $cur_trust = $request->trust;
 $response = array();
 $cur_ques;
@@ -53,3 +57,33 @@ if ($_SESSION["user_id"] == $user_id) {
 }
 $conn->close();
 echo json_encode($response);
+=======
+// $ques_id = $request->ques_id;
+
+$response = array();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    $sql = "SELECT * FROM `users` WHERE `user_id` = '" . $user_id . "';";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $curr = $row["cur_ques"];
+            $pts = $row["points"] - 5;
+            if ($curr < 5) {
+                $update = "UPDATE `users` SET `cur_ques` = " . 2 . ", `points` = " . $pts . "  WHERE `user_id` = '" . $user_id . "';";
+            } else if ($curr > 5 && $curr < 9) {
+                $update = "UPDATE `users` SET `cur_ques` = " . 5 . ", `points` = " . $pts . "  WHERE `user_id` = '" . $user_id . "';";
+            }
+
+            mysqli_query($conn, $update);
+            $response["message"] = "eureka bitch";
+        }
+    } else {
+        $response["message"] = "womp womp womp";
+    }
+    echo json_encode($response);
+}
+$conn->close();
+>>>>>>> 6279259c638314100928366b0120f5b5b2e3cf6f
